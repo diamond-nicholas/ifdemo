98d8193849eb3ba90ec5fdc80da9ad4284c3a5d8
diamond-nicholas <diamondnicholas154@gmail.com>
2022-01-22 10:47:15 +0100

    add top navbar

--- client/ifdemo/src/Component/Navbar/Navbar.js
+++ client/ifdemo/src/Component/Navbar/Navbar.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import './Navbar.scss';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsBell } from 'react-icons/bs';
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <nav className='navbar'>
+      <div className='nav-container'>
+        {/* nav top */}
+        <div className='nav-top'>
+          <div className='logo'>
+            <img
+              src='https://i.pinimg.com/originals/3f/1e/87/3f1e8798a4627a4801656f5b8c061545.png'
+              alt=''
+            />
+          </div>
+          <ul className='nav-list'>
+            <li className='msg'>
+              <div className='iconMsg'>
+                <HiOutlineMail />
+              </div>
+              <p>Message</p>
+            </li>
+            <li>
+              <div className='iconNotif'>
+                <BsBell />
+              </div>
+              <p>Notification</p>
+            </li>
+            <li>
+              <div className='card'>
+                <div className='card-body'>
+                  <p>Mr. Nick</p>
+                  <button className='card-btn'>Log out</button>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* nav bottom */}
+      </div>
+    </nav>
   );
 };
 

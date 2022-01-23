f6aa4192b219b761bb86b55d1cf76ed3e39b88b8
diamond-nicholas <diamondnicholas154@gmail.com>
2022-01-23 11:01:10 +0100

    add grid area to menu, style first card

--- client/ifdemo/src/App.js
+++ client/ifdemo/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Current from './Component/Pages/Current';
+import Main from './Component/Main/Main';
 
 function App() {
   return (
     <Router className=''>
       <Navbar />
+      <Main />
       <Routes>
-        <Route exact path='/' element={<Current />} />
+        <Route exact path='/current' element={<Current />} />
       </Routes>
     </Router>
   );
---
+++ client/ifdemo/src/Component/Main/Main.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <section className='card'>
+        <div className='card-body top'>
+          <div className='card-content'>
+            <h3>Welcome to Intelligent Finance</h3>
+            <p>
+              We’re here to support our existing customers in any way we can.
+            </p>
+            <button>Ways to get in touch</button>
+          </div>
+        </div>
+        {/* 2 */}
+        <div className='card-body sidebar1'>
+          <h3>Welcome to Intelligent Finance</h3>
+          <p>We’re here to support our existing customers in any way we can.</p>
+          <button>Ways to get in touch</button>
+        </div>
+        {/* 3 */}
+        <div className='card-body sidebar2'>
+          <h3>Welcome to Intelligent Finance</h3>
+          <p>We’re here to support our existing customers in any way we can.</p>
+          <button>Ways to get in touch</button>
+        </div>
+        {/* 4 */}
+        <div className='card-body sidebar3'>
+          <h3>Welcome to Intelligent Finance</h3>
+          <p>We’re here to support our existing customers in any way we can.</p>
+          <button>Ways to get in touch</button>
+        </div>
+        {/* 5 */}
+        <div className='card-body sidebar4'>
+          <h3>Welcome to Intelligent Finance</h3>
+          <p>We’re here to support our existing customers in any way we can.</p>
+          <button>Ways to get in touch</button>
+        </div>
+        {/* 6 */}
+        <div className='card-body sidebar5'>
+          <h3>Welcome to Intelligent Finance</h3>
+          <p>We’re here to support our existing customers in any way we can.</p>
+          <button>Ways to get in touch</button>
+        </div>
+        {/* footer */}
+        <div className='card-body bottom'>
+          <h3>Welcome to Intelligent Finance</h3>
+          <p>We’re here to support our existing customers in any way we can.</p>
+          <button>Ways to get in touch</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Main;
